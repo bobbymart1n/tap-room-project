1a4b12f828cc0f621044279b5432db9ae9bcc81f
Bobby Martin and Nick Kardish <bobby@>
2018-05-02 09:43:49 -0700

    Add add new keg toggle, add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,31 @@ import { Keg } from './models/keg.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  visible: boolean = false;
   kegs: Keg[] = [
     new Keg('RPM' ,'Boneyard', 6, 6.5),
     new Keg('Breakside IPA', 'Breakside', 6, 6.2),
     new Keg('Mirror Pond Pale Ale', 'Deschutes', 5, 5)
   ];
+  toggle() {
+    this.visible = !this.visible;
+  }
   selectedKeg = null;
   displayKeg(clickedKeg) {
     this.selectedKeg = clickedKeg;
   }
+
+  // addNewKeg = null;
+  // newKeg(addClickedKeg) {
+  //   this.addNewKeg = true;
+  // }
   addKeg(newName, newBrand, newPrice, newAlcoholContent){
     let keg = new Keg(newName, newBrand, newPrice, newAlcoholContent);
     this.kegs.push(keg);
+    // this.addNewKeg = null;
+  }
+  editKeg = null;
+  displayEdit(clickedKeg) {
+    this.editKeg = clickedKeg;
   }
 }
